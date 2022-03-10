6306669a4257ea59d25f3afe92db0c7f5a0cbe5d
Cynamide <arjitagarwal123@gmail.com>
2022-03-10 18:28:23 +0530

    removed ssl command

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+// eslint-disable-next-line
 import { Drawer, Button } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import "./Cart.css";
 
 function Cart() {
+  // eslint-disable-next-line
   const [visible, setVisible] = useState(false);
 
   const showDrawer = () => {
     setVisible(true);
   };
-
+  // eslint-disable-next-line
   const onClose = () => {
     setVisible(false);
   };
