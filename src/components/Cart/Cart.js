0af7b85badacb269b5332aee95f5bed7d78c86f0
Cynamide <arjitagarwal123@gmail.com>
2022-04-30 04:33:51 +0530

    frontend done with integration maam please give us full marks

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -25,10 +25,127 @@ function Cart() {
     viewCart();
     setVisible(true);
   };
+  const onAddMed = (id) => {
+    console.log("id:", id);
+    const token = localStorage.getItem("access-token");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_URL}/user/add_to_cart/${id}`,
+        {},
+        config
+      )
+      .then((res) => {
+        // const arr = [...cartData];
+        // //eslint-disable-next-line
+        // let obj = arr.find((o, i) => {
+        //   if (o.id === id) {
+        //     arr[i]["quantity"] = res.data.quantity;
+        //     return true; // stop searching
+        //   }
+        // });
+        // setCartData(arr);
+        // console.log(arr);
+        console.log(res.data);
+        viewCart();
+      })
+      .catch((err) => {
+        console.log(err.data);
+      });
+  };
+
+  const onDelete = (id) => {
+    const token = localStorage.getItem("access-token");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    axios
+      .delete(
+        `${process.env.REACT_APP_BACKEND_URL}/user/remove_from_cart/${id}`,
+        config
+      )
+      .then((res) => {
+        // const arr = [...cartData];
+        // //eslint-disable-next-line
+        // let obj = arr.find((o, i) => {
+        //   if (o.id === id) {
+        //     arr.splice(i, 1);
+        //     return true; // stop searching
+        //   }
+        // });
+        // setCartData(arr);
+        // console.log(arr);
+        viewCart();
+      })
+      .catch((err) => {
+        console.log(err.data);
+      });
+  };
+
+  const onSubMed = (id) => {
+    const token = localStorage.getItem("access-token");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_URL}/user/remove_from_cart/${id}`,
+        {},
+        config
+      )
+      .then((res) => {
+        viewCart();
+      })
+      .catch((err) => {
+        console.log(err.data);
+      });
+  };
+
+  const redirectToMedPage = (id) => {
+    navigate.push("/medicine?id=" + id);
+  };
   // eslint-disable-next-line
   const onClose = () => {
     setVisible(false);
   };
+
+  const checkOutBackend = () => {
+    const token = localStorage.getItem("access-token");
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/checkout/`, config)
+      .then((res) => {
+        setVisible(true);
+      })
+      .catch((err) => {
+        console.log(err.data);
+      });
+  };
+
   const checkOut = () => {
     // const data = await fetch("http://localhost:1337/razorpay", {
     //   method: "POST",
@@ -50,6 +167,7 @@ function Cart() {
         if (!response.razorpay_payment_id || response.razorpay_payment_id < 1) {
           navigate.push("/payment-failed");
         } else {
+          checkOutBackend();
           navigate.push("/payment-success");
         }
         console.log(response);
@@ -88,6 +206,13 @@ function Cart() {
         console.log(err.data);
       });
   };
+  const total = () => {
+    let sum = 0;
+    cartData.map((item) => {
+      sum += item.price * item.quantity;
+    });
+    return sum;
+  };
 
   return (
     <>
@@ -101,49 +226,78 @@ function Cart() {
         onClose={onClose}
         visible={visible}
         extra={
-          <Button type="primary" onClick={checkOut}>
-            Checkout
-            <ArrowRightOutlined />
-          </Button>
+          <div style={{ display: "flex", marginTop: "0.5rem" }}>
+            <p style={{ marginRight: "1rem", fontSize: "1rem" }}>
+              Total: ₹ {total()}{" "}
+            </p>
+            <Button type="primary" onClick={checkOut}>
+              Checkout
+              <ArrowRightOutlined />
+            </Button>
+          </div>
         }
       >
         <Row gutter={[16, 16]}>
-          {cartData.map((med) => (
-            <Col span={12}>
-              <Card
-                style={{ width: 200 }}
-                className="cart-card"
-                cover={
-                  <img
-                    alt="example"
-                    className="cart-image"
-                    src={med.image_url}
-                  />
-                }
-                actions={[
-                  <Popover
-                    placement="bottom"
-                    content={removeMed}
-                    trigger="hover"
+          {cartData.map(
+            (med) =>
+              med.quantity > 0 && (
+                <Col span={12}>
+                  <Card
+                    style={{ width: 200 }}
+                    className="cart-card"
+                    cover={
+                      <img
+                        alt="example"
+                        className="cart-image"
+                        onClick={() => redirectToMedPage(med.product)}
+                        src={med.image_url}
+                      />
+                    }
+                    actions={[
+                      <Popover
+                        placement="bottom"
+                        content={removeMed}
+                        trigger="hover"
+                      >
+                        <CloseCircleOutlined
+                          onClick={() => onDelete(med.product)}
+                          key="remove"
+                        />
+                      </Popover>,
+                      <Popover
+                        placement="bottom"
+                        content={subMed}
+                        trigger="hover"
+                      >
+                        <MinusOutlined
+                          onClick={() => onSubMed(med.product)}
+                          key="subQty"
+                        />
+                      </Popover>,
+                      <b>{med.quantity}</b>,
+                      <Popover
+                        placement="bottom"
+                        content={addMed}
+                        trigger="hover"
+                      >
+                        <PlusOutlined
+                          onClick={() => onAddMed(med.product)}
+                          key="addQty"
+                        />
+                      </Popover>,
+                    ]}
                   >
-                    <CloseCircleOutlined key="remove" />
-                  </Popover>,
-                  <Popover placement="bottom" content={subMed} trigger="hover">
-                    <MinusOutlined key="subQty" />
-                  </Popover>,
-                  <b>{med.quantity}</b>,
-                  <Popover placement="bottom" content={addMed} trigger="hover">
-                    <PlusOutlined key="addQty" />
-                  </Popover>,
-                ]}
-              >
-                {med.name}
-                <br />
-                <br />
-                <p>₹ {med.price}</p>
-              </Card>
-            </Col>
-          ))}
+                    {med.name}
+                    <br />
+                    <br />
+                    <p>
+                      ₹ {med.price} x {med.quantity} = ₹
+                      {med.price * med.quantity}
+                    </p>
+                  </Card>
+                </Col>
+              )
+          )}
         </Row>
       </Drawer>
     </>
